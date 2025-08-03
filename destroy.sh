39429da617a0c770aef18511ddddf7c76090978d
Damien Barrett <damienbarrett@hotmail.com>
2025-08-03 17:31:18 +1200

    fix: syntax error

--- destroy.sh
+++ destroy.sh
@@ -1,5 +1,5 @@
 sudo nix --extra-experimental-features "nix-command flakes" \
   run github:nix-community/disko -- \
   --mode destroy,format,mount \
-  --flake github:damienbarrett#single-ext4-destructive \
+  --flake github:damienbarrett/omanix#single-ext4-destructive \
   --argstr device /dev/nvme0n1
