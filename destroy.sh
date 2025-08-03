4d516b9d548df6eba1194dc388c6d67d507bffb9
Damien Barrett <damienbarrett@hotmail.com>
2025-08-03 21:44:55 +1200

    fix: remove flakes (for now)

--- destroy.sh
+++ destroy.sh
@@ -1,5 +1,6 @@
-sudo nix --extra-experimental-features 'nix-command flakes' \
-  run github:nix-community/disko -- \
-  --mode destroy,format,mount \
-  --flake 'github:damienbarrett/omanix#single-disk-ext4-destructive' \
-  --argstr device /dev/nvme0n1
+git clone https://github.com/damienbarrett/omanix
+cp ~/omanix/hosts/vmnix/scripts/single-disk-ext4-destructive/single-disk-ext4-destructive.sh /tmp/disk-config.sh
+cp ~/omanix/hosts/vmnix/scripts/single-disk-ext4-destructive/single-disk-ext4-destructive.nix /tmp/disk-config.nix
+sudo /tmp/disk-config.sh
+sudo nixos-generate-config --root /mnt
+sudo nixos-install
