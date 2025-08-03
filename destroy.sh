eb0add1c1a90f9da40e36104016c751db1ef48bb
Damien Barrett <damienbarrett@hotmail.com>
2025-08-03 22:14:50 +1200

    add: root password reset reminder

--- destroy.sh
+++ destroy.sh
@@ -2,14 +2,6 @@ git clone https://github.com/damienbarrett/omanix
 cp ~/omanix/hosts/vmnix/scripts/single-disk-ext4-destructive/single-disk-ext4-destructive.sh /tmp/disk-config.sh
 cp ~/omanix/hosts/vmnix/scripts/single-disk-ext4-destructive/single-disk-ext4-destructive.nix /tmp/disk-config.nix
 sudo /tmp/disk-config.sh
-sudo nixos-generate-config --root /mnt
-sudo nixos-install
-echo Next Steps
-echo ========== 
-echo Open /etc/nixos/configuration.nix
-echo - Enable sshd
-echo - Add vim and wget
-echo - Add alice
-echo Set a password for alice
-echo - passwd alice
-echo sudo reboot
+sudo nixos-generate-config --root /mnt && nixos-install
+
+echo ensure root password is set and then reboot
