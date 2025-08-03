35a1c4878b76b77cba59a5cb31e5db0892e92b50
Damien Barrett <damienbarrett@hotmail.com>
2025-08-03 22:31:04 +1200

    add: password change

--- destroy.sh
+++ destroy.sh
@@ -3,3 +3,7 @@ cp ~/omanix/hosts/vmnix/scripts/single-disk-ext4-destructive/single-disk-ext4-de
 cp ~/omanix/hosts/vmnix/scripts/single-disk-ext4-destructive/single-disk-ext4-destructive.nix /tmp/disk-config.nix
 sudo /tmp/disk-config.sh
 sudo nixos-generate-config --root /mnt && nixos-install
+# set password - doesn't seem to happen interactively when running the above
+sudo nixos-enter --root /mnt
+passwd        # set root's password
+exit
