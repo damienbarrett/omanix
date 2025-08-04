0710d3dd8b97d648a35d9c0dfb60fb1ac4e1ea22
Damien Barrett <damienbarrett@hotmail.com>
2025-08-04 21:07:05 +1200

    fix: relative path to fixed path for destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,9 +1,7 @@
+cd /tmp
 git clone https://github.com/damienbarrett/omanix
-cp ~/omanix/hosts/vmnix/scripts/single-disk-ext4-destructive/single-disk-ext4-destructive.sh /tmp/disk-config.sh
-cp ~/omanix/hosts/vmnix/scripts/single-disk-ext4-destructive/single-disk-ext4-destructive.nix /tmp/disk-config.nix
+cp /tmp/omanix/hosts/vmnix/scripts/single-disk-ext4-destructive/single-disk-ext4-destructive.sh /tmp/disk-config.sh
+cp /tmp/omanix/hosts/vmnix/scripts/single-disk-ext4-destructive/single-disk-ext4-destructive.nix /tmp/disk-config.nix
 sudo /tmp/disk-config.sh
-sudo nixos-generate-config --root /mnt && nixos-install
-# set password - doesn't seem to happen interactively when running the above
-sudo nixos-enter --root /mnt
-passwd        # set root's password
-exit
+sudo nixos-generate-config --root /mnt 
+# sudo nixos-install
