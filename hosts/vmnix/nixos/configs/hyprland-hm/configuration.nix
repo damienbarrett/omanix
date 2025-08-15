023df7123368ea4b1516507046bf0d30da556bcc
Damien Barrett <damienbarrett@hotmail.com>
2025-08-15 21:06:49 +1200

    rev: Revert back to greeter user

--- hosts/vmnix/nixos/configs/hyprland-hm/configuration.nix
+++ hosts/vmnix/nixos/configs/hyprland-hm/configuration.nix
@@ -116,7 +116,7 @@
     enable = true;
     settings.default_session = {
       command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
-      user = "nixos"; # your user
+      user = "greeter"; 
     };
   };
 
