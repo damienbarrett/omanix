b6b7e6b929e261752825d8fb5b2e9eed875560f3
Damien Barrett <damienbarrett@hotmail.com>
2025-08-15 14:43:32 +1200

    add: hyprland home manager

---
+++ hosts/vmnix/nixos/configs/hyprland-hm/configuration.nix
@@ -0,0 +1,149 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  boot.loader.systemd-boot.enable = true; # Use the systemd-boot EFI boot loader.
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "vmnixos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.nixos = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+     packages = with pkgs; [
+       tree
+     ];
+   };
+
+  # programs.firefox.enable = true;
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+   environment.systemPackages = with pkgs; [
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     wget
+   ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+   programs.gnupg.agent = {
+     enable = true;
+     enableSSHSupport = true;
+   };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+   services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+  # NEW THINGS
+
+  # Enable experimental features such as flakes.
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.download-buffer-size = 268435456; # increase buffer download size to 256MB. Default is 64 and caused issues.
+  # Hyprland system integration (session file, polkit, xdp, xwayland, etc.)
+  programs.hyprland.enable = true;  # required system-level piece
+
+  # Simple text TUI login (replace with SDDM/GDM if you prefer)
+  services.greetd = {
+    enable = true;
+    settings.default_session = {
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+      user = "greeter";
+    };
+  };
+
+  # XDG portals (screensharing, file pickers) for Hyprland
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-hyprland
+    ];
+    config = {
+      common.default = [ "gtk" ];
+      hyprland.default = [ "gtk" "hyprland" ];
+    };
+  };
+
+  # Audio (PipeWire)
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    jack.enable = true;
+  };
+
+  # Fonts so Waybar icons look right
+  fonts.packages = with pkgs; [ font-awesome nerdfonts ];
+}
+
---
+++ hosts/vmnix/nixos/configs/hyprland-hm/home.nix
@@ -0,0 +1,94 @@
+# home.nix
+{ config, pkgs, ... }:
+
+{
+  # Minimum required by Home Manager:
+  home.username = "nixos";
+  home.homeDirectory = "/home/nixos";
+  home.stateVersion = "25.05";  # pick the version you first start with; don't bump casually
+
+  home.packages = with pkgs; [
+    bash-completion
+	bat
+	btop
+	curl
+	eza
+	fastfetch
+	fd
+	fzf
+	git
+	gh
+	inetutils
+	less
+	jq
+	lazygit
+	man
+	nushell
+	plocate
+	ripgrep
+	neovim
+	tldr
+	unzip
+	whois
+	yazi
+	zoxide
+  ];
+
+
+
+
+  #Hyprland via HM (actual compositor config)
+  wayland.windowManager.hyprland = {
+    enable = true;
+    # systemd target is on by default; keep it (it exports env vars to user services)
+    # wayland.windowManager.hyprland.systemd.enable = true;
+
+    settings = {
+      "$mod" = "SUPER";
+      monitor = ",preferred,auto,1";
+
+      # Start essentials once
+      exec-once = [
+        "waybar"
+        "lxqt-policykit-agent"  # polkit GUI
+        "mako"                  # notifications
+        "swaybg -i /run/current-system/sw/share/backgrounds/nixos/nix-wallpaper-simple-blue.png"
+      ];
+
+      bind = [
+        "$mod, Return, exec, kitty"
+        "$mod, D, exec, wofi --show drun"
+        "$mod, Q, killactive,"
+        "$mod SHIFT, E, exit,"
+        "$mod, F, fullscreen,"
+      ];
+    };
+  };
+
+  # Bar
+  programs.waybar = {
+    enable = true;
+    # super-minimal bar
+    settings = [{
+      layer = "top";
+      position = "top";
+      modules-left = [ "hyprland/workspaces" ];
+      modules-center = [ "clock" ];
+      modules-right = [ "pulseaudio" "network" "cpu" "memory" "battery" ];
+    }];
+  };
+
+  # Launcher / terminal / notifications
+  programs.wofi.enable = true;         # Wayland app launcher
+  programs.kitty.enable = true;        # terminal
+  services.mako.enable = true;         # notify daemon
+
+  # Polkit agent (so auth prompts appear on Hyprland)
+  services.lxqt-policykit-agent.enable = true;
+
+  # Handy Wayland tools
+  home.packages = with pkgs; [
+    wl-clipboard grim slurp swaybg
+    xdg-utils
+  ];
+}
