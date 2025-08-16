ccfd1fe64c3644fa6a951e895ac3909c2019c2ef
Damien Barrett <damienbarrett@hotmail.com>
2025-08-16 22:14:38 +1200

    add: open-vm-tools

--- hosts/vmnix/nixos/configs/hyprland-hm/configuration.nix
+++ hosts/vmnix/nixos/configs/hyprland-hm/configuration.nix
@@ -59,6 +59,7 @@
    environment.systemPackages = with pkgs; [
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
+	 open-vm-tools
    ];
 
   # Some programs need SUID wrappers, can be configured further or are
