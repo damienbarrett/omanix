6629115cabee4e2700d22ced814bf09a2c3e4486
Damien Barrett <damienbarrett@hotmail.com>
2025-08-21 20:03:26 +1200

    ref: add echo statements

--- scripts/vmware-fusion-display/t1-check-drivers.sh
+++ scripts/vmware-fusion-display/t1-check-drivers.sh
@@ -1,3 +1,5 @@
+echo "check drivers are in place"
+echo "--------------------------"
 echo "ls /run/opengl-driver/lib/dri | grep -E 'vmwgfx|svga'"
 ls /run/opengl-driver/lib/dri | grep -E 'vmwgfx|svga'
 # → vmwgfx_dri.so  (present)
--- scripts/vmware-fusion-display/t2-check-glx-uses-vmwgfx.sh
+++ scripts/vmware-fusion-display/t2-check-glx-uses-vmwgfx.sh
@@ -1,3 +1,7 @@
+echo "Check GLX uses vmwgfx"
+echo "------------"
+
+echo "LIBGL_DEBUG=verbose glxinfo -B | egrep 'driver|OpenGL (renderer|version)'"
 LIBGL_DEBUG=verbose glxinfo -B | egrep 'driver|OpenGL (renderer|version)'
 # Expect to see it load vmwgfx_dri.so and report:
 # OpenGL renderer string: SVGA3D …
