f8839c310d296e41e5f98fe914a676a21924d7a9
Damien Barrett <damienbarrett@hotmail.com>
2025-08-30 19:53:40 +1200

    try: modules by hostname

--- hosts/vmnix/hyprland-hm/flake.nix
+++ hosts/vmnix/hyprland-hm/flake.nix
@@ -2,28 +2,31 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    mkSystem = { host, system }:
+    nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./hosts/${host}/configuration.nix
+        ./hosts/${host}/hardware-configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.nixos = import ./hosts/${host}/home.nix;
+        }
+      ];
+      specialArgs = { inherit inputs; }; # handy if modules need inputs
+    };
+  in {
     nixosConfigurations = {
-      vmnixos = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.nixos = ./home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
-        ];
-      };
+      vmnixos = mkSystem { host = "vmnixos"; system = "aarch64-linux"; };
     };
   };
 }
