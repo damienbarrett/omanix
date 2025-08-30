d961c214c585aa77292455bcf803b892102ee130
Damien Barrett <damienbarrett@hotmail.com>
2025-08-30 20:23:10 +1200

    try: graphical greet

--- hosts/vmnix/hyprland-hm/hosts/vmnixos/configuration.nix
+++ hosts/vmnix/hyprland-hm/hosts/vmnixos/configuration.nix
@@ -6,130 +6,70 @@
   boot.loader.systemd-boot.enable = true; # Use the systemd-boot EFI boot loader.
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "vmnixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.hostName = "vmnixos"; 
 
-  # Set your time zone.
   # time.timeZone = "Europe/Amsterdam";
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
+  # i18n/default locale, console, X11 left as-is (commented)
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.nixos = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
-       tree
-     ];
-   };
-
-  # programs.firefox.enable = true;
+  users.users.nixos = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+  };
 
   # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-   environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
-      wlr-randr wayland-utils xorg.xrdb pciutils
+  environment.systemPackages = with pkgs; [
 
-   open-vm-tools
-   mesa-demos
-   ];
+    # system utilities
+    vim
+    wget
 
-  # services.openvmtools.enable = true;
+    # desktop environment
+    wlr-randr
+    wayland-utils
+    xorg.xrdb
+    pciutils
+
+    # drivers and utilities
+    mesa-demos
+    open-vm-tools
+  ];
+
+  # VMware guest integration
   virtualisation.vmware.guest.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-   programs.gnupg.agent = {
-     enable = true;
-     enableSSHSupport = true;
-   };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
-  # List services that you want to enable:
+  services.openssh.enable = true;
 
-  # Enable the OpenSSH daemon.
-   services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
 
   # NEW THINGS
 
   # Enable experimental features such as flakes.
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.download-buffer-size = 268435456; # increase buffer download size to 256MB. Default is 64 and caused issues.
+  nix.settings.download-buffer-size = 268435456;
+
   # Hyprland system integration (session file, polkit, xdp, xwayland, etc.)
-  # programs.hyprland.enable = true;  # required system-level piece
   programs.hyprland = {
     enable = true;
-	xwayland.enable = true;
+    xwayland.enable = true;
   };
+
+  # login/greeter
 
   services.greetd = {
     enable = true;
-    settings.default_session = {
-      command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
-      user = "greeter"; 
-    };
   };
+
+  programs.regreet.enable = true;
 
   hardware.graphics.enable = true;
 
@@ -155,39 +95,21 @@
     jack.enable = true;
   };
 
-  # Fonts so Waybar icons look right
-  # fonts.packages = with pkgs; [ font-awesome nerdfonts ];
-  # Fonts so Waybar icons look right
+  # Waybar Fonts
   fonts.packages = with pkgs; [
     font-awesome
-    # nerd-fonts.jetbrains-mono
     nerd-fonts.monaspace
-    # or: nerd-fonts.fira-code, nerd-fonts.droid-sans-mono, etc.
   ];
+
+  # VMware + wlroots env vars for 
   environment.variables = {
-    WLR_RENDERER_ALLOW_SOFTWARE = "1";  # wlroots: allow llvmpipe
-    XWAYLAND_NO_GLAMOR = "1";           # Xwayland: no glamor/GL
-    LIBGL_ALWAYS_SOFTWARE = "1";        # force software GL just in case
+    WLR_RENDERER_ALLOW_SOFTWARE = "1";
+    XWAYLAND_NO_GLAMOR = "1";
+    LIBGL_ALWAYS_SOFTWARE = "1";
   };
-  #
-  # # System daemon (host/guest glue)
-  # systemd.services.vmtoolsd = {
-  #   description = "VMware Tools Daemon";
-  #   wantedBy = [ "multi-user.target" ];
-  #   after = [ "network.target" ];
-  #   serviceConfig.ExecStart = "${pkgs.open-vm-tools}/bin/vmtoolsd -b";
-  # };
-  #
-  # # Per-user helper (auto-resize, clipboard for X11/XWayland apps)
-  # systemd.user.services.vmusr = {
-  #   description = "VMware Tools user agent";
-  #   wantedBy = [ "graphical-session.target" ];
-  #   partOf   = [ "graphical-session.target" ];
-  #   serviceConfig.ExecStart = "${pkgs.open-vm-tools}/bin/vmtoolsd -n vmusr";
-  # };
+
   boot.kernelModules = [ "vmwgfx" ];
   services.xserver.videoDrivers = [ "vmware" "modesetting" ];
 
   nixpkgs.overlays = [ (import ./overlays/mesa-svga.nix) ];
 }
-
